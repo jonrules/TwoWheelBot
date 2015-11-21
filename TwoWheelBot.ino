ae2465135f8d95236b85d7c4801950390c32ee1c
Jonathan Baltazar <jonrules@gmail.com>
2015-11-21 17:59:27 -0600

    implement accelerometer y-axis

--- TwoWheelBot.ino
+++ TwoWheelBot.ino
@@ -1,6 +1,7 @@
-/* Include gyro libraries */
+/* Include sensor libraries */
 #include <Wire.h>
 #include <L3G.h>
+#include <LSM303.h>
 
 /* Include pololu maestro libraries */
 #include <PololuMaestro.h>
@@ -15,19 +16,24 @@ pin 11 to transmit (TX). */
   SoftwareSerial maestroSerial(10, 11);
 #endif
 
-/* Include math libraries */
+/* Include utils libraries */
 #include <math.h>
 
 
 /* Gyro */
 L3G gyro;
-int maxmDps = 100;
+int maxmDps = 500;
 int gyroSensitivity = 500;
 float gyroScale = 8.75;
 float gyroMax = (float)maxmDps/gyroScale;
 int gyroXDirection = 1;
 int gyroYDirection = -1;
 int gyroZDirection = -1;
+
+/* Compass */
+LSM303 compass;
+int accelerometerYSensitivity = 100;
+int accelerometerYDirection = -1;
 
 /* Maestro servo controller */
 MiniMaestro maestro(maestroSerial);
@@ -41,8 +47,7 @@ int servoZero = (int)((servoMax + servoMin)/2);
 void setup() {
   Serial.begin(9600);
   Wire.begin();
-  maestroSerial.begin(9600);
-
+  
   if (!gyro.init(L3G::deviceType::device_D20H, L3G::sa0State::sa0_high))
   {
     delay(2000);
@@ -54,8 +59,14 @@ void setup() {
     delay(2000);
     Serial.println("Gyro initialized!");
   }
-
   gyro.enableDefault();
+
+  compass.init();
+  compass.enableDefault();
+  compass.m_min = (LSM303::vector<int16_t>){-32767, -32767, -32767};
+  compass.m_max = (LSM303::vector<int16_t>){+32767, +32767, +32767};
+
+  maestroSerial.begin(9600);
 }
 
 int calculateRotationOffset(int gyroValue) {
@@ -63,27 +74,51 @@ int calculateRotationOffset(int gyroValue) {
   return servoOffset;
 }
 
+int calculateAccelerometerOffset(int accelerometerValue) {
+  return accelerometerValue;
+}
+
+int getSafeServoPosition(int servoPosition) {
+  if (servoPosition > servoMax) {
+    return servoMax;
+  }
+  if (servoPosition < servoMin) {
+    return servoMin;
+  }
+  return servoPosition;
+}
+
 void loop() {
   gyro.read();
+  compass.read();
   
-
   int leftServoPosition = servoZero;
   int rightServoPosition = servoZero;
 
-  if (gyro.g.y > gyroSensitivity) 
+  if (abs(gyro.g.y) > gyroSensitivity) 
   {
     int yRotationOffset = calculateRotationOffset((int)gyro.g.y);
     leftServoPosition += gyroYDirection*yRotationOffset;
     rightServoPosition -= gyroYDirection*yRotationOffset;
-    Serial.print("Gyro.y:");
-    Serial.print((int)gyro.g.y);
-    Serial.print(", Left:");
-    Serial.print(leftServoPosition);
-    Serial.print(",Right:");
-    Serial.println(rightServoPosition);
   }
 
-  maestro.setTarget(leftServo, leftServoPosition);
-  maestro.setTarget(rightServo, rightServoPosition);
+  if (abs(compass.a.y) > accelerometerYSensitivity)
+  {
+    int yAccelerometerOffset = calculateAccelerometerOffset((int)compass.a.y);
+    leftServoPosition += accelerometerYDirection*yAccelerometerOffset;
+    rightServoPosition -= accelerometerYDirection*yAccelerometerOffset;
+  }
+
+  Serial.print("Gyro.g.y:");
+  Serial.print((int)gyro.g.y);
+  Serial.print(", Compass.a.y:");
+  Serial.print((int)compass.a.y);
+  Serial.print(", Left:");
+  Serial.print(leftServoPosition);
+  Serial.print(",Right:");
+  Serial.println(rightServoPosition);
+
+  maestro.setTarget(leftServo, getSafeServoPosition(leftServoPosition));
+  maestro.setTarget(rightServo, getSafeServoPosition(rightServoPosition));
 }
 
